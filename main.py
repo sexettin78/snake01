447fc451fe640429ed2eea1652cdcd854ded8f5f
Sexettin <72075007+sexettin78@users.noreply.github.com>
2021-09-04 18:13:33 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+import  turtle
+import time
+
+delay = 0.1
+
+wn = turtle.Screen()
+wn.title("Yılan oyunu yapımcı Sexettin")
+wn.bgcolor("green")
+wn.setup(width=600,height=600)
+wn.tracer(0)
+
+
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("red")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+def go_up():
+    head.direction = "up"
+
+def go_down():
+    head.direction = "down"
+
+def go_left():
+    head.direction = "left"
+    
+def go_right():
+    head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20) 
+        
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.ycor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.ycor()
+        head.setx(x + 20)
+
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+while True:
+    wn.update()
+
+    move()
+    
+    time.sleep(delay)
+wn.mainloop()
